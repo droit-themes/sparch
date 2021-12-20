512a81bbca46fde9a5dfbc10187dc2500dd219a4
nayeem-droitlab <nayeem@droitlab.com>
2021-12-20 18:19:01 +0600

    update

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,12 +10,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    
-	<?php
-    if ( get_post_type( get_the_ID() ) != 'team' ) {
-        sparch_post_thumbnail();
-     }
-    ?>
 
 	<div class="entry-content">
 		<?php
