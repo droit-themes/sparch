99cb3fc8fd211e16718368d1ea106442332f34ed
Abdur Rahaman <79562059+dlab10@users.noreply.github.com>
2022-09-01 11:34:27 +0600

    update sparch

--- footer.php
+++ footer.php
@@ -9,9 +9,7 @@
  * @package sparch
  */
 $footer_text = sparch_options('sparch_copyright_txt', 'Copyright &copy; 2021 <a href="#">DroitThemes</a> | All rights reserved');
-
 ?>
-
 	<footer id="colophon" class="site-footer">
 		<div class="site-info container">
 			<?php echo sparch_kses($footer_text); ?>
