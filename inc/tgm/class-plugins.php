afa0dfcae963e23af2e675e382d936d632d512ea
Omar Faruk <omarfaruk@droitlab.com>
2024-06-11 15:53:56 +0600

    update

--- inc/tgm/class-plugins.php
+++ inc/tgm/class-plugins.php
@@ -26,15 +26,27 @@ class sparch_Register_Plugins {
 			),
 	
 			array(
-				'name'               => esc_html__( 'Droit Addons For Elementor', 'sparch' ),
-				'slug'               => 'droit-elementor-addons',
-				'required'           => true,
+				'name'               => esc_html__( 'Droit Addons For Elementor', 'sparch' ), // The plugin name.
+				'slug'               => 'droit-elementor-addons', // The plugin slug (typically the folder name).
+				'source'             => 'https://saasland.droitlab.com/downloadfile/droit-elementor-addons_3.1.5.zip', // The plugin source.
+				'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+				'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+				'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+				'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+				'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+				'version'            => ''
 			),
-	
+
 			array(
-				'name'               => esc_html__( 'Droit Dark Mode', 'sparch' ),
-				'slug'               => 'droit-dark-mode',
-				'required'           => true,
+				'name'               => esc_html__( 'Droit Dark', 'sparch' ), // The plugin name.
+				'slug'               => 'droit-dark-mode', // The plugin slug (typically the folder name).
+				'source'             => 'https://dlcarspa.droitlab.com/wp-content/plugins/droit-dark-mode.zip', // The plugin source.
+				'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+				'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+				'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+				'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+				'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+				'version'            => ''
 			),
 	
 			array(
